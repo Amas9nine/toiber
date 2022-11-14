332fd8e160598017f87a94e9d1ab68d4aec79548
Turdubaevajazgul <jazgulnvk@gmail.com>
2022-11-14 16:52:39 +0300

    feat: footer layout and  adaptive

---
+++ src/constants/FooterLinks.jsx
@@ -0,0 +1,56 @@
+export const HelpLinks = [
+  {
+    title: "FAQs",
+    id: 1
+  },
+  {
+    title: "Map",
+    id: 2
+  },
+  {
+    title: "Recommendations",
+    id: 3
+  },
+
+  {
+    title: "Comments",
+    id: 4
+  },
+
+  {
+    title: "Benefits",
+    id: 5
+  }
+];
+export const ServiceLinks = [
+  {
+    title: "Restaurants",
+    id: 1
+  },
+  {
+    title: "Decor",
+    id: 2
+  },
+  {
+    title: "Filming",
+    id: 3
+  },
+
+  {
+    title: "Stage",
+    id: 4
+  },
+
+  {
+    title: "Leading",
+    id: 5
+  },
+  {
+    title: "Wardrobe",
+    id: 5
+  },
+  {
+    title: "Tuple",
+    id: 5
+  }
+];
